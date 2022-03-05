9354cd796869bd674104422e56a10efaa7331b49
bibek77 <bibekrana01@gmail.com>
2022-03-05 22:16:05 +0530

    main screen done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:instant_messenger/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +12,9 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
           child: Column(
         children: [
+          Spacer(flex: 2),
           Image.asset("assets/images/welcome-image.png"),
+          Spacer(flex: 3,),
           Text(
             "Hi! Lets Sphere Chat",
             textAlign: TextAlign.center,
@@ -22,12 +25,39 @@ class HomeScreen extends StatelessWidget {
           ),
           Text(
             "Quick chat with anyone with Sphere Chat.",
+            textAlign: TextAlign.center,
             style: TextStyle(
                 color: Theme.of(context)
                     .textTheme
                     .bodyText1
                     ?.color!
                     .withOpacity(0.64)),
+          ),
+          Spacer(flex: 3,),
+          FittedBox(
+            child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      "Skip",
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.color
+                              ?.withOpacity(0.8)),
+                    ),
+                    SizedBox(width: kDefaultPadding/4,),
+                    Icon(Icons.arrow_forward_ios,
+                        size: 16,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .color
+                            ?.withOpacity(0.8))
+                  ],
+                )),
           )
         ],
       )),
