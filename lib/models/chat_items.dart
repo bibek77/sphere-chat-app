dde18e47573d25af9412993591293367f4254454
bibek77 <bibekrana01@gmail.com>
2022-03-08 22:56:20 +0530

    chat messages added

--- lib/models/chat_items.dart
+++ lib/models/chat_items.dart
@@ -18,37 +18,65 @@ List chatData = [
 
 ChatItems(
   name: "Anos Voldigoad",
-  lastMessage: "Did you watch that Anime?",
-  image: "assets/images/johndoe_1.png",
+  lastMessage: "Did you watch my Anime?",
+  image: "assets/images/profile_images/anos-voligoad-profile.jpeg",
   time: "2m ago",
   isActive: false
 ),
 ChatItems(
   name: "Edward Elric",
   lastMessage: "My brother and I are going on a trip.",
-  image: "assets/images/johndoe_2.png",
+  image: "assets/images/profile_images/edward-profile.jpeg",
   time: "8m ago",
   isActive: false
 ),
 ChatItems(
   name: "Bokuto",
   lastMessage: "hey hey hey !!!",
-  image: "assets/images/johndoe_3.png",
+  image: "assets/images/profile_images/bokuto-profile.jpeg",
   time: "2d ago",
   isActive: false
 ),
 ChatItems(
   name: "Zeke Yeager",
   lastMessage: "My plan is going to be successful. Want in ?",
-  image: "assets/images/johndoe_4.png",
+  image: "assets/images/profile_images/zeke-yeager-profile.jpeg",
   time: "4d ago",
   isActive: false
 ),
 ChatItems(
   name: "Genos",
   lastMessage: "Gonna repair my arm today.",
-  image: "assets/images/johndoe_5.png",
+  image: "assets/images/profile_images/genos-profile.jpeg",
   time: "10d ago",
+  isActive: false
+),
+ChatItems(
+  name: "Baki",
+  lastMessage: "Thanks",
+  image: "assets/images/profile_images/baki-profile.jpeg",
+  time: "10d ago",
+  isActive: false
+),
+ChatItems(
+  name: "Gaara",
+  lastMessage: "Need a sand refill.",
+  image: "assets/images/profile_images/gaara-profile.jpeg",
+  time: "12d ago",
+  isActive: false
+),
+ChatItems(
+  name: "Zenitsu",
+  lastMessage: "Nezuko Chaaaaaaan !",
+  image: "assets/images/profile_images/zenitsu-profile.jpeg",
+  time: "15d ago",
+  isActive: false
+),
+ChatItems(
+  name: "Senku",
+  lastMessage: "One billion percent I am sure.",
+  image: "assets/images/profile_images/senku-profile.jpeg",
+  time: "17d ago",
   isActive: false
 ),
 
