ae9ac478292f578b80c375c5ee651c51b16d4dcc
bibek77 <bibekrana01@gmail.com>
2022-03-10 22:32:20 +0530

    message screen added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,26 +2,55 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instant_messenger/screens/chat_body.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  int _selectedIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       body: ChatBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(CupertinoIcons.person_add_solid,
-        color: Colors.white,),
+        onPressed: () {},
+        child: Icon(
+          CupertinoIcons.person_add_solid,
+          color: Colors.white,
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.chat_bubble_2_fill), label: "Chats"),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_2_fill), label: "People"),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.phone_fill), label: "Calls"),
-        ],
-      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble_2_fill), label: "Chats"),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_2_fill), label: "People"),
+        BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.phone_fill), label: "Calls"),
+        BottomNavigationBarItem(
+          icon: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage(
+                  "assets/images/profile_images/senku-profile.jpeg")),
+          label: "Profile",
+        ),
+      ],
     );
   }
 
--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -4,22 +4,22 @@ import 'package:instant_messenger/utils/constants.dart';
 
 ThemeData lightThemeData(BuildContext context) {
   return ThemeData.light().copyWith(
-    primaryColor: kPrimaryColor,
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: appBarTheme,
-    iconTheme: IconThemeData(color: kContentLightColor),
-    colorScheme: ColorScheme.light(
-      primary: kPrimaryColor,
-      secondary: kSecondaryColor,
-      error: kErrorColor,
-    ),
-    textTheme: GoogleFonts.interTextTheme(
-        Theme.of(context).textTheme.apply(bodyColor: kContentLightColor)),
-    // bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    //   selectedItemColor: kPrimaryColor,
-    //   selectedIconTheme: IconThemeData(color: kPrimaryColor),
-    // )
-  );
+      primaryColor: kPrimaryColor,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: appBarTheme,
+      iconTheme: IconThemeData(color: kContentLightColor),
+      colorScheme: ColorScheme.light(
+        primary: kPrimaryColor,
+        secondary: kSecondaryColor,
+        error: kErrorColor,
+      ),
+      textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme.apply(bodyColor: kContentLightColor)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+          selectedItemColor: kContentLightColor.withOpacity(0.7),
+          unselectedItemColor: kContentLightColor.withOpacity(0.32),
+          selectedIconTheme: IconThemeData(color: kPrimaryColor)));
 }
 
 ThemeData darkThemeData(BuildContext context) {
@@ -37,9 +37,10 @@ ThemeData darkThemeData(BuildContext context) {
           Theme.of(context).textTheme.apply(bodyColor: kContentDarkColor)),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
           backgroundColor: kContentLightColor,
-          selectedItemColor: Colors.white,
+          selectedItemColor: kContentDarkColor.withOpacity(0.7),
           unselectedItemColor: kContentDarkColor.withOpacity(0.32),
-          selectedIconTheme: IconThemeData(color: Colors.white)));
+          selectedIconTheme: IconThemeData(color: kPrimaryColor)));
 }
 
-final appBarTheme = AppBarTheme(centerTitle: false, elevation: 0, color: kPrimaryColor);
+final appBarTheme =
+    AppBarTheme(centerTitle: false, elevation: 0, color: kPrimaryColor);
